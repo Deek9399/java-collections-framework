f559f779bef8815d5221dab3ea2291a05ab93e7a
Deek9399 <deeksha2021@gmail.com>
2025-02-21 14:25:42 -0500

    fixed arraylist demo

--- src/com/java/collection/arraylist/ArrayListClasDemo.java
+++ src/com/java/collection/arraylist/ArrayListClasDemo.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class ArrayListClasDemo {
     public static void main(String[] args) {
-        List<Integer> fruits = new ArrayList<>(20);
+        List<String> fruits = new ArrayList<>(20);
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Grapes");
+        fruits.add(1,"Pear"); // adds pear at first index
+        fruits.set(2,"Strawberr"); //replaces element
+
+        System.out.println(fruits);
     }
 }
